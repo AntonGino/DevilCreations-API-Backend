c2034d868ea18c38a1eea44fc64ec2e025210d84
J.A ANTON GINO <antonginoja200@gmail.com>
2025-08-22 17:09:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import Flask, request, jsonify
 import os
 import requests
 from flask_cors import CORS
-CORS(app, origins=["https://antongino.github.io"])
+
 
 app = Flask(__name__)
+CORS(app, origins=["https://antongino.github.io"])
 HF_API_KEY = os.environ.get("HF_API_KEY")
 
 @app.route("/proxy", methods=["POST"])
